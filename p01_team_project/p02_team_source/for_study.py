8862a91e023ce73d34a1fe6230eb0e264007bdb8
MOOSUNGPARK <sunkid100000@gmail.com>
2017-05-22 02:56:06 +0900

    z

---
+++ p01_team_project/p02_team_source/for_study.py
@@ -0,0 +1,27 @@
+'''
+
+
+# 야구게임
+1) https://youtu.be/45CxKN0O6UI
+
+
+# @property
+
+1) http://brownbears.tistory.com/160
+2) https://www.programiz.com/python-programming/property
+
+
+# attack()
+1) 함수 작동 방식 
+
+# Team.init_player()
+1) 어떤 자료가 들어가나
+
+# Game 클래스 변수 설정한 이유는?
+
+# Game.advance_setting()
+1) 함수 작동 방식 
+2) 왜 range는 큰 수에서 작은 수로 loop 돌게 했나
+
+
+'''
