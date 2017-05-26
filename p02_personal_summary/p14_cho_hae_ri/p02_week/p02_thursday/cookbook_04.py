c9893646f3283d37774a6b936535b38b3fa91c29
irb <gow1231@naver.com>
2017-05-26 18:51:51 +0900

    T

--- p02_personal_summary/p14_cho_hae_ri/p02_week/p02_thursday/cookbook_04.py
+++ p02_personal_summary/p14_cho_hae_ri/p02_week/p02_thursday/cookbook_04.py
@@ -407,7 +407,392 @@ with open('c:/data/...') as f:
 
 # 대개의 경우, 순환에 for 문을 사용하지만 보다 더 정교한 조정이 필요한 때도 있다.
 # 기저에서 어떤 동작이 일어나는지 정확히 알아 둘 필요가 있다.
-# 다음 상호작용하는 예제를 통해 순환하는 동안 기볹거으로 어떤 일이 일어나는지 알아보자.
+# 다음 상호작용하는 예제를 통해 순환하는 동안 기본적으로 어떤 일이 일어나는지 알아보자.
+
+
+items = [1,2,3]
+
+# 이터레이터 얻기
+it = iter(items)  # items.__iter__() 실행
+
+# 이터레이터 실행
+next(it)          # it.__nex__() 실행
+#1
+
+next(it)
+#2
+
+next(it)
+#3
+
+next(it)
+'''
+  File "<ipython-input-25-a0d1ced6d968>", line 20, in <module>
+    next(it)
+StopIteration
+
+'''
+
+
+
+########################## 4.2 델리게이팅 순환 ###########################
+
+# 리스트, 튜플 등 순환 가능한 객체를 담은 사용자 정의 컨테이너를 만들었다.
+# 이 컨테이너에 사용 가능한 이터레이터(iterator) 를 만들고 싶다.
+
+# 해결 - 일반적으로 컨테이너 순환에 사용할 __inter__() 메소드만 정의해 주면 된다.
+
+
+class Node:
+    def __init__(self, value):
+        self._value = value
+        self._children = []
+
+    def __repr__(self):
+        return 'Node({!r})'.format(self._value)
+
+    def add_child(self, node):
+        self._children.append(node)
+
+    def __iter__(self):
+        return iter(self._children)
+
+# Example
+if __name__ == '__main__':
+    root = Node(0)
+    child1 = Node(1)
+    child2 = Node(2)
+    root.add_child(child1)
+    root.add_child(child2)
+    # Outputs Node(1), Node(2)
+    for ch in root:
+        print(ch)
+
+# 이 코드에서 __inter__() 가 실제 순환을 수행하기 위한 __next__() 메소드를 구현하는 특별 이터레이터 객체를
+# 반환하기를 요구한다.
+# 만약 다른 컨테이너에 들어 있는 내용물에 대한 순환이 해야 할 작업의 전부라면, 이터레이터의 동작 방식을 완전히
+# 이해할 필요는 없다. 이때는 요청 받은 순환을 다음으로 전달하기만 하면 된다.
+
+#iter() 함수에 대한 사용은 코드를 깔끔하게 하는 지름길과 같다. iter(s) 는 단순히 s.__iter__() 를 호출해서 이터레이터를 반환하는데,
+# 이는 len(s)가 s.__len__() 을 호출하는 것과 같은 방식이다.
+
+
+
+
+############################ 4.3. 제너레이터로 새로운 순환 패턴 생성 ##############################
+
+# 문제 : 내장함수 range(), reversed() 와는 다른 동작을 하는 순환 패턴을 만들고 싶다.
+
+# 해결 : 새로운 순환 패턴을 만들고 싶다면 제너레이터 함수를 사용해서 정의해야 된다.
+
+
+def frange(start, stop, increment):
+    x = start
+    while x < stop:
+        yield x
+        x += increment
+
+# 이런 함수를 사용하려면, for 순환문이나 순환 객체를 소비하는 다른 함수 sum(), list() 등을 사용한 순환을 해야 한다.
+
+for n in frange(0, 4, 0.5):
+    print(n)
+
+'''
+0
+0.5
+1.0
+1.5
+2.0
+2.5
+3.0
+3.5
+'''
+
+list(frange(0,1,0.125))
+# [0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875]
+
+# 내부의 yield 문의 존재로 인해 함수가 제너레이터가 되었다. 일반 함수와는 다르게 제너레이터는 순환에 응답하기 위해 실행된다.
+# 이런 함수가 어떻게 동작하는지 다음 예를 통해 알아보자.
+
+def countdown(n):
+    print("Starting to count from", n)
+    while n >0:
+        yield n
+        n-= 1
+    print('Done! ')
+
+# 제너레이터 생성, 아무런 출력물이 없음에 주목한다.
+
+c = countdown(3)
+
+c
+# <generator object countdown at 0x0000000004C20830>
+
+# 값을 만들기 위한 첫 번째 실행
+next(c)
+#Starting to count from 3
+#Out[30]:
+#3
+
+# 다음 값을 위한 실행
+next(c)
+#2
+
+# 다음 값을 위한 실행
+next(c)
+#1
+
+# 다음 값을 위한 실행(순환 종료)
+next(c)
+
+'''
+Done! 
+Traceback (most recent call last):
+  File "C:\Users\Administrator\Anaconda3\lib\site-packages\IPython\core\interactiveshell.py", line 2881, in run_code
+    exec(code_obj, self.user_global_ns, self.user_ns)
+  File "<ipython-input-33-73b012f9653f>", line 1, in <module>
+    next(c)
+StopIteration
+
+'''
+
+# 중요한 점은 제너레이터 함수가 순환에 의한 "다음" 연산에 응답하기 위해서만 실행된다는 점이다.
+# 제너레이터 함수가 반환되면 순환을 종료한다.
+
+
+
+
+#############################  4.4. 이터레이터 프로토콜 구현  ###############################
+
+# 순환을 지원하는 객체를 만드는데, 이터레이터 프로토콜을 구현하는 쉬운 방법이 필요하다.
+
+# 해결 - 객체에 대한 순환을 가장 쉽게 구현하는 방법은 제너레이터 함수를 사용하는 것이다.
+# 레시피 4.2 에서 트리 구조를 표현하기 위해 node 클래스를 사용했다.
+# 노드를 깊이 - 우선 패턴으로 순환하는 이터레이터를 구현하고 싶다면 다음 코드를 참고한다.
+
+
+class Node:
+    def __init__(self, value):
+        self._value = value
+        self._children = []
+
+    def __repr__(self):
+        return 'Node({!r})'.format(self._value)
+
+    def add_child(self, node):
+        self._children.append(node)
+
+    def __iter__(self):
+        return iter(self._children)
+
+    def depth_first(self):
+        yield self
+        for c in self:
+            yield from c.depth_first()
+
+# 예제
+
+if __name__ == '__main__':
+    root = Node(0)
+    child1 = Node(1)
+    child2 = Node(2)
+    root.add_child(child1)
+    root.add_child(child2)
+    child1.add_child(Node(3))
+    child1.add_child(Node(4))
+    child2.add_child(Node(5))
+
+    for ch in root.depth_first():
+        print(ch)
+
+# 이 코드에서 depth_first() 메소드는 지고간저긍로 읽고 이해할 수 있다. 처음에는 자기 자신을 만들고(yield)
+# 그 다음에는 자식을 순환한다.
+# 이때 그 자식은 depth_first() 메소드로 아이템을 만든다.
+
+
+
+# 토론
+
+# 파이썬의 이터레이터 프로토콜은 __iter__() 가 __next__() 메소드를 구현하고 종료를 알리기 위해 StopIteration 예외를 사용하는
+#  특별 이터레이터 객체를 반환하기르 ㄹ요구한다.
+# 다음 코드는 관련 이터레이터 클래스를 사용한 depth_first() 메소드의 대안 구현법을 보여준다.
+
+
+
+
+class Node:
+    def __init__(self, value):
+        self._value = value
+        self._children = []
+
+    def __repr__(self):
+        return 'Node({!r})'.format(self._value)
+
+    def add_child(self, other_node):
+        self._children.append(other_node)
+
+    def __iter__(self):
+        return iter(self._children)
+
+    def depth_first(self):
+        return DepthFirstIterator(self)
+
+class DepthFirstIterator(object):
+    '''
+    Depth-first traversal
+    '''
+
+    def __init__(self, start_node):
+        self._node = start_node
+        self._children_iter = None
+        self._child_iter = None
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        # Return myself if just started; create an iterator for children
+        if self._children_iter is None:
+            self._children_iter = iter(self._node)
+            return self._node
+        # If processing a child, return its next item
+        elif self._child_iter:
+            try:
+                nextchild = next(self._child_iter)
+                return nextchild
+            except StopIteration:
+                self._child_iter = None
+                return next(self)
+        # Advance to the next child and start its iteration
+        else:
+            self._child_iter = next(self._children_iter).depth_first()
+            return next(self)
+
+# DepthFirstIterator 클래스는 제너레이터를 사용한 것과 동일하게 동작한다. 하지만 순환하는 동안 생기는
+# 복잡한 상황을 처리하기 위해 코드가 꽤 지저분하다.
+
+
+
+
+#################################  4.5. 역방향 순환  #####################################
+
+# 문제 - 시퀀스 아이템을 역방향으로 순환하고 싶다면
+
+# 해결 - 내장함수 reversed() 를 사용한다.
+
+
+
+a = [1, 2, 3, 4]
+for x in reversed(a):
+    print(x)
+
+'''
+4
+3
+2
+1
+'''
+
+
+# 역방향 순환은 객체가 __reversed__() 특별 메소드를 구현하고 있거나 크기를 알 수 있는 경우에만 가능하다.
+# 두 조건 중 아무것도 만족하지 못하면 객체를 먼저 리스트로 변환해야 한다.
+
+# 파일을 거꾸로 출력하기
+
+f = open('somefile')
+for line in reversed(list(f)):
+    print(line, end='')
+
+
+# 하지만 순환 가능한 객체를 리스트로 변환할 때 많은 메모리가 필요하다는 점은 주의!!
+
+
+# 토론
+# __reversed__() 메소드를 구현하면 사용자 정의 클래스에서 역방향 순환이 가능하다는 점을 많은 프로그래머들이 모르고 있다.
+
+
+class Countdown:
+    def __init__(self, start):
+        self.start = start
+
+    # Forward iterator
+    def __iter__(self):
+        n = self.start
+        while n > 0:
+            yield n
+            n -= 1
+
+    # Reverse iterator
+    def __reversed__(self):
+        n = 1
+        while n <= self.start:
+            yield n
+            n += 1
+
+for rr in reversed(Countdown(30)):
+    print(rr)
+for rr in Countdown(30):
+    print(rr)
+
+
+# 역방향 이터레이터를 정의하면 코드를 훨씬 효율적으로 만들어 주고, 데이터를 리스트로 변환하고 순환하는 수고를 덜어준다.
+
+
+
+
+####################  4.6. 추가 상태를 가진 제너레이터 함수 정의  ########################
+
+
+# 문제 - 제너레이터 함수를 정의하고 싶지만, 사용자에게 노출할 추가적인 상태를 넣고 싶다.
+
+# 해결 - 사용자에게 추가 상태를 노출하는 제너레이터를 원할 때, __iter__() 메소드에 제너레이터 함수 코드를 넣어서
+# 쉽게 클래스로 구현할 수 있다느 점을 기억하자.
+
+
+from collections import deque
+
+class linehistory:
+    def __init__(self, lines, histlen=3):
+        self.lines = lines
+        self.history = deque(maxlen=histlen)
+
+    def __iter__(self):
+        for lineno, line in enumerate(self.lines, 1):
+            self.history.append((lineno, line))
+            yield line
+
+    def clear(self):
+        self.history.clear()
+
+# 이 클래스를 사용하려면 일반 제너레이터 함수처럼 대해야 한다. 하지만 인스턴스를 만들기 때문에 history 속성이나
+# clear() 메소드 같은 내부 속성에 접근할 수 있다.
+
+
+with open('somefile.txt') as f:
+    lines = linehistory(f)
+    for line in lines:
+        if 'python' in line:
+            for lineno, hline in lines.history:
+                print('{}:{}'.format(lineno, hline), end='')
+
+
+# 제너레이터를 사용하면 모든 작업을 함수만으로 하려는 유혹에 빠지기 쉽다.
+# 만약 제너레이터 함수가 프로그램의 다른 부분과 일반적이지 않게 상호작용해야 할 경우 코드가 꽤 복잡해질 수 있다..
+
+f = open('somefile.txt')
+lines = linehistory(f)
+next(lines)
+#Traceback (most recent call last):
+#    File "<stdin>", line 1, in <module>
+#TypeError: 'linehistory' object is not an iterator
+
+# Call iter() first, then start iterating
+it = iter(lines)
+next(it)
+#'hello world\n'
+next(it)
+#'this is a test\n'
+
 
 
 
